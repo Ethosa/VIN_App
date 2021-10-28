4f3b18fab71b803eeb805f0727daeefd07e9cf02
user2 <user2@CLASS31005>
2021-10-28 15:28:08 +0700

    Добавьте файлы проекта.

---
+++ REG_MARK_LIB/RegMark.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace REG_MARK_LIB
+{
+    public class RegMark
+    {
+        public Boolean CheckMark(String mark)
+        {
+            return false;
+        }
+
+        public String GetNextMarkAfter(String mark)
+        {
+            return "";
+        }
+
+        public String GetNextMarkAfterInRange(String prevMark, String rangeStart, String rangeEnd)
+        {
+            return "";
+        }
+
+        public int GetCombinationsCountInRange(String mark1, String mark2)
+        {
+            return 0;
+        }
+    }
+}
---
+++ VIN_LIB/VIN.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VIN_LIB
+{
+    public class VIN
+    {
+        public Boolean CheckVIN(String vin)
+        {
+            return false;
+        }
+
+        public String GetVINCountry(String vin)
+        {
+            return "";
+        }
+
+        public int GetTransportYear(String vin)
+        {
+            return 0;
+        }
+    }
+}
