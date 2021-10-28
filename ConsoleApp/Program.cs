8663a223dad3c1c67772448411fe50ab9ac970b1
user2 <user2@CLASS31005>
2021-10-28 16:45:32 +0700

    ad console project

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using VIN_LIB;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VIN vin = new VIN();
+            Console.WriteLine(vin.CheckVIN("JH4KB16535L011820"));
+            Console.WriteLine(vin.CheckVIN("KLAJB82Z2XK338143"));
+        }
+    }
+}
--- VIN_LIB/VIN.cs
+++ VIN_LIB/VIN.cs
@@ -10,7 +10,7 @@ namespace VIN_LIB
             Regex rx = new Regex(
                 @"^(?<wmi>[a-z1-9]{3})(?<vds>[a-z0-9]{5})(?<sign>[0-9x]{1})(?<vsi>[a-z0-9]{8})(?<![OQI])$",
                 RegexOptions.IgnoreCase | RegexOptions.Compiled);
-            return false;
+            return rx.Match(vin).Success;
         }
 
         public String GetVINCountry(String vin)
