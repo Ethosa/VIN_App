d6a0b78345b3a79618fe3d3f54d1e69f10ec3f2e
renardein <drleo2001@gmail.com>
2021-10-31 19:50:23 +0700

    sync

--- WindowsApp/Auth.Designer.cs
+++ WindowsApp/Auth.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace WindowsApp
 {
-    partial class Form1
+    partial class Auth
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -29,13 +29,33 @@ namespace WindowsApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Auth
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(434, 231);
+            this.Controls.Add(this.button1);
+            this.Name = "Auth";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WindowsApp/Auth.cs
+++ WindowsApp/Auth.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WindowsApp
 {
-    public partial class Form1 : Form
+    public partial class Auth : Form
     {
-        public Form1()
+        public Auth()
         {
             InitializeComponent();
         }
