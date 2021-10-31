c98bf5e62406ad2bd25ae8238a329bddfb4962f1
Ethosa <social.ethosa@gmail.com>
2021-10-31 13:01:04 +0700

    update GetCountryVIN method

--- VIN_LIB/VIN.cs
+++ VIN_LIB/VIN.cs
@@ -8,8 +8,12 @@ namespace VIN_LIB
     public class VIN
     {
         // Словарь содержит в себе модельные годы автомобилей, начиная с 1980-го
-        // A: 1980, B: 1981, etc.
+        // A: 1980; B: 1981, etc.
         private Dictionary<char, int> yearsModel = new Dictionary<char, int>();
+
+        // Словарь countries содержит в себе информацию о странах по их кодам.
+        // AA: ЮАР; 1A: США, etc.
+        private Dictionary<string, string> countries = new Dictionary<string, string>();
         
         // Регулярка ниже парсит VIN и режет на отдельные куски.
         // wmi - World Manufacturers entification
@@ -17,14 +21,91 @@ namespace VIN_LIB
         // sign - Control sign
         // modelYear - vehicle model year
         // vis - vehicle identification section
-        private Regex vin_rule = new Regex(
+        private Regex vinRule = new Regex(
             @"^(?<wmi>[a-hj-npr-z1-9]{3})(?<vds>[a-hj-npr-z0-9]{5})(?<sign>[0-9x]{1})(?<modelYear>[a-hj-npr-tv-y1-9]{1})(?<vis>[a-hj-npr-z0-9]{7})$",
             RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        // Сгенерированные коды стран с их названиями.
+        private string countryCodes =
+            "AA-AH ЮАРAJ-AN Котд'Ивуар;BA-BE Ангола;BF-BK Кения;BL-BR Танзания;CA-CE Бенин;CF-CK Мадагаскар;CL-CR Тунис;DA-DE " +
+            "Египет;DF-DK Марокко;DL-DR Замбия;EA-EE Эфиопия;EF-EK Мозамбик;FA-FE Гана;FF-FK Нигерия;JA-JT Япония;KA-KE " +
+            "ШриЛанка;KF-KK Израиль;KL-KR Южная Корея;KS-K0 Казахстан;LA-L0 Китай;MA-ME Индия;MF-MK Индонезия;ML-MR " +
+            "Таиланд;NF-NK Пакистан;NL-NR Турция;PA-PE Филиппины;PF-PK Сингапур;PL-PR Малайзия;RA-RE ОАЭ;RF-RK Тайвань;RL-RR " +
+            "Вьетнам;RS-R0 Саудовска я Аравия;SA-SM Великобритани я;SN-ST Германия;SU-SZ Польша;S1-S4 Латвия;TA-TH " +
+            "Швейцария;TJ-TP Чехия;TR-TV Венгрия;TW-T1 Португалия;UH-UM Дания;UN-UT Ирландия;UU-UZ Румыния;U5-U7 " +
+            "Словакия;VA-VE Австрия;VF-VR Франция;VS-VW Испания;VX-V2 Сербия;V3-V5 Хорватия;V6-V0 Эстония;WA-W0 " +
+            "Германия;XA-XE Болгария;XF-XK Греция;XL-XR Нидерланды;XS-XW СССР/СНГ;XX-X2 Люксембург;X3-X0 Россия;YA-YE " +
+            "Бельгия;YF-YK Финляндия;YL-YR Мальта;YS-YW Швеция;YX-Y2 Норвегия;Y3-Y5 Беларусь;Y6-Y0 Украина;ZA-ZR Италия;" +
+            "1A-10 США;2A-20 Канада;3A-3W Мексика;3X-37 Коста Рика;38-30 Каймановы острова;4A-40 США;5A-50 США;6A-6W " +
+            "Австралия;7A-7E Новая Зеландия;8A-8E Аргентина;8F-8K Чили;8L-8R Эквадор;8S-8W Перу;8X-82 Венесуэла;9A-9E " +
+            "Бразилия;9F-9K Колумбия;9L-9R Парагвай;9S-9W Уругвай;9X-92 Тринидад и Тобаго;93-99 Бразилия;ZX-Z2 " +
+            "Словения;Z3-Z5 Литва;Z6-Z0 Россия";
 
         /// <summary>
         /// Простой конструктор, заполняющий словарь yearsModel при инициализации.
         /// </summary>
         public VIN()
+        {
+            FillYears();
+            FillCountries();
+        }
+
+        /// <summary>
+        /// Проверяет валидность VIN
+        /// </summary>
+        /// <param name="vin">VIN, требующий проверки</param>
+        /// <returns>true, если VIN валиден</returns>
+        public Boolean CheckVIN(String vin)
+        {
+            return vinRule.Match(vin).Success;
+        }
+
+        /// <summary>
+        /// Получает страну-изготовитель транспорта
+        /// </summary>
+        /// <param name="vin"></param>
+        /// <returns></returns>
+        public String GetVINCountry(String vin)
+        {
+            string countryCode = GetValue(vinRule.Match(vin), "wmi");
+            return "";
+        }
+
+        /// <summary>
+        /// Получает год изготовления транспорта
+        /// </summary>
+        /// <param name="vin">VIN, прошедший проверку регуляркой</param>
+        /// <returns>год от 1980</returns>
+        public int GetTransportYear(String vin)
+        {
+            return GetTransportYear(vinRule.Match(vin));
+        }
+
+        /// <summary>
+        /// Разбирает значения, полученные с matched.
+        /// </summary>
+        /// <param name="matched">VIN, прошедший проверку регуляркой</param>
+        /// <param name="key">Требуемое значение</param>
+        /// <returns>Полученное значение</returns>
+        private String GetValue(Match matched, String key)
+        {
+            if (matched.Success)
+                return matched.Groups[key].Value;
+            return "";
+        }
+
+        private int GetTransportYear(Match matched)
+        {
+            if (!matched.Success)
+                return -1;
+            char val = matched.Groups["modelYear"].Value[0];
+            return yearsModel[val];
+        }
+
+        /// <summary>
+        /// Заполняет словарь yearsModel.
+        /// </summary>
+        private void FillYears()
         {
             int year = 1980;
             for (int i = 65; i <= 89; ++i)  // A..Z
@@ -42,49 +123,20 @@ namespace VIN_LIB
         }
 
         /// <summary>
-        /// Проверяет валидность VIN
+        /// Заполняет словарь countries.
         /// </summary>
-        /// <param name="vin">VIN, требующий проверки</param>
-        /// <returns>true, если VIN валиден</returns>
-        public Boolean CheckVIN(String vin)
+        private void FillCountries()
         {
-            return vin_rule.Match(vin).Success;
-        }
+            string[] codes = countryCodes.Split(';');
+            foreach (string code in codes)
+            {
+                string[] codeInfo = code.Split(' ', 2);
+                countries.Add(
+                    codeInfo[0][0] + "[" + codeInfo[0][1] + "-" + codeInfo[0][4] + "]",
+                    codeInfo[1]);
+            }
 
-        /// <summary>
-        /// Получает страну-изготовитель транспорта
-        /// </summary>
-        /// <param name="vin"></param>
-        /// <returns></returns>
-        public String GetVINCountry(String vin)
-        {
-            return "";
-        }
-
-        public int GetTransportYear(String vin)
-        {
-            return GetTransportYear(vin_rule.Match(vin));
-        }
-
-        /// <summary>
-        /// Разбирает значения, полученные с matched.
-        /// </summary>
-        /// <param name="matched">VIN, прошедший проверку регуляркой</param>
-        /// <param name="key">Требуемое значение</param>
-        /// <returns>Полученное значение</returns>
-        private String getValue(Match matched, String key)
-        {
-            if (matched.Success)
-                return matched.Groups[key].Value;
-            return "";
-        }
-
-        private int GetTransportYear(Match matched)
-        {
-            if (!matched.Success)
-                return -1;
-            char val = matched.Groups["modelYear"].Value[0];
-            return yearsModel[val];
+            Console.WriteLine(countries["S[N-T]"]);
         }
     }
 }
