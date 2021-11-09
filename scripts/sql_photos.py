50d9bdc7af1ec1066b4b10e45dd510388648a6e6
user2 <user2@CLASS31005>
2021-11-09 13:16:13 +0700

    update `scripts :eyes:

--- scripts/sql_photos.py
+++ scripts/sql_photos.py
@@ -10,19 +10,24 @@ cnct = pydb.connect("Driver={SQL Server Native Client 11.0};"
 cursor = cnct.cursor()
 
 
-# for i, filename in enumerate(os.listdir("../docs/Ресурсы/Сессия 2/drivers 2/photo")):
-# 	print(i+1, filename)
-# 	path = f"D:/Ethosa/vinapp/vin_app/docs/Ресурсы/Сессия 2/drivers 2/photo/{filename}"
-# 	with open(path, 'rb') as f:
-# 		data = f.read()
-# 	cursor.execute(
-# 		"INSERT INTO driverPhoto (filename, image_data) VALUES (?, ?)", (filename,data))
-# 	cursor.commit()
+for i, filename in enumerate(os.listdir("../docs/Ресурсы/Сессия 2/drivers 2/photo")):
+	print(i+1, filename)
+	path = f"D:/Ethosa/vinapp/vin_app/docs/Ресурсы/Сессия 2/drivers 2/photo/{filename}"
+	with open(path, 'rb') as f:
+		data = f.read().replace(b'\x00', b'\x99\x11')
+	cursor.execute(
+		"INSERT INTO driverPhoto (filename, image_data) VALUES (?, ?)", (filename,data))
+	cursor.commit()
 
-print(cursor.execute("""SELECT TOP (1000) [pid]
-      ,[filename]
-      ,[image_data]
-  FROM [gibdd].[dbo].[driverPhoto]""").fetchall())
+# print(cursor.execute("""SELECT TOP (1000) [pid]
+#       ,[filename]
+#       ,[image_data]
+#   FROM [gibdd].[dbo].[driverPhoto]""").fetchall())
+
+# translate to png
+# with open('img.png', 'wb') as ff:
+# 	ff.write(data.replace(b'\x99\x11', b'\x00'))
+
 
 cursor.close()
 cnct.close()
