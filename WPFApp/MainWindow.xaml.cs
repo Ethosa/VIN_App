09f94fc09dc8f61532fc1c600924512289170743
курсы <user2@ktt.ru>
2021-11-10 12:51:10 +0700

    rewrite WPF :eyes:

--- WPFApp/MainWindow.xaml.cs
+++ WPFApp/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,76 +16,54 @@ using System.Windows.Shapes;
 namespace WPFApp
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        /// <summary>
-        /// Количество неправильных попыток входа.
-        /// </summary>
         private int wrongAuthCount = 0;
-
         public MainWindow()
         {
             InitializeComponent();
-            Loaded += new RoutedEventHandler(MainWindow_Loaded);
+            Loaded += new RoutedEventHandler(OnCreate);
         }
 
-        /// <summary>
-        /// Событие, срабатывающее при загрузке окна.
-        /// <para>Подгружает таймаут.</para>
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="args"></param>
-        public async void MainWindow_Loaded(object sender, RoutedEventArgs args)
+        private async void OnCreate(object sender, RoutedEventArgs args)
         {
             int timeout = Properties.Settings.Default.timeout;
             if (timeout > 0)
-                wrongAuthCount = 3;
-            await AttemptsTimer(timeout);
-        }
-
-        /// <summary>
-        /// Событие, срабатывающее при нажатии на кнопку входа.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private async void ButtonOnClick(object sender, RoutedEventArgs e)
-        {
-            errorLabel.Text = null;
-            if (string.IsNullOrEmpty(authLogin.Text) || string.IsNullOrEmpty(authPassword.Text))
-                errorLabel.Text = "Заполните Логин и Пароль!";
-            else
             {
-                if (wrongAuthCount < 3)
-                    ++wrongAuthCount;
-                else
-                    await AttemptsTimer(60);
+                wrongAuthCount = 3;
+                await AttemtsTimer(timeout);
             }
         }
 
-        /// <summary>
-        /// Блокировка входа на определенное время.
-        /// </summary>
-        /// <param name="sec">Timeout seconds<see cref="int"/>.</param>
-        /// <returns>The <see cref="Task"/>.</returns>
-        private async Task AttemptsTimer(int timeout)
+        private async Task AttemtsTimer(int timeout)
         {
             errorLabel.Text = $"Совершено {wrongAuthCount} неудачных попытки входа. Вход заблокирован на {timeout} секунд";
             authButton.IsEnabled = false;
             while (timeout > 0)
                 timeout = await UpdateTimeout(timeout);
-            errorLabel.Text = null;
             authButton.IsEnabled = true;
+            errorLabel.Text = null;
             wrongAuthCount = 0;
+        }
+
+        private async void AuthButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(authLogin.Text) || string.IsNullOrEmpty(authPassword.Text))
+                return;
+            else
+            {
+                return;
+            }
         }
 
         private async Task<int> UpdateTimeout(int timeout)
         {
-            errorLabel.Text = $"Совершено {wrongAuthCount} неудачных попытки входа. Вход заблокирован на {timeout} секунд";
-            --timeout;
-            SaveTimeout(timeout);
             await Task.Delay(1000);
+            --timeout;
+            errorLabel.Text = $"Совершено {wrongAuthCount} неудачных попытки входа. Вход заблокирован на {timeout} секунд";
+            SaveTimeout(timeout);
             return timeout;
         }
 
