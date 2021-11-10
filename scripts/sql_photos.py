904498fdf60405f32e721cfca357d3608d31362b
Ethosa <social.ethosa@gmail.com>
2021-11-10 21:31:01 +0700

    ...

--- scripts/sql_photos.py
+++ scripts/sql_photos.py
@@ -10,14 +10,24 @@ cnct = pydb.connect("Driver={SQL Server Native Client 11.0};"
 cursor = cnct.cursor()
 
 
+
+
 for i, filename in enumerate(os.listdir("../docs/Ресурсы/Сессия 2/drivers 2/photo")):
-	print(i+1, filename)
-	path = f"D:/Ethosa/vinapp/vin_app/docs/Ресурсы/Сессия 2/drivers 2/photo/{filename}"
-	with open(path, 'rb') as f:
-		data = f.read()
-	# cursor.execute(
-	# 	"INSERT into driverPhoto (filename, image_data) values (?, ?)", (filename, data))
-	# cursor.commit()
+    print(i+1, filename)
+    path = f"../docs/Ресурсы/Сессия 2/drivers 2/photo/{filename}"
+    with open(path, 'rb') as f:
+        data = f.read()
+        imgdata = ''
+        for i in data:
+        	if i != 0:
+        		imgdata += chr(i)
+        	else:
+        		imgdata += '000'
+        print(imgdata)
+    cursor.execute(
+        "INSERT into driverPhoto (pid, filename, image_data) values (?, ?, ?)", (1, filename, imgdata))
+    cursor.commit()
+    break
 
 # print(cursor.execute("""SELECT TOP (1000) [pid]
 #       ,[filename]
@@ -26,7 +36,7 @@ for i, filename in enumerate(os.listdir("../docs/Ресурсы/Сессия 2/d
 
 # translate to png
 # with open('img.png', 'wb') as ff:
-# 	ff.write(data.replace(b'\x99\x11', b'\x00'))
+#     ff.write(data.replace(b'\x99\x11', b'\x00'))
 
 cursor.close()
 cnct.close()
