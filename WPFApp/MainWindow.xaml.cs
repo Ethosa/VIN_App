749711b799b3306c5c336d523111f493bf3e4b6e
Ethosa <social.ethosa@gmail.com>
2021-11-10 10:17:04 +0700

    update wpf

--- WPFApp/MainWindow.xaml.cs
+++ WPFApp/MainWindow.xaml.cs
@@ -38,9 +38,12 @@ namespace WPFApp
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="args"></param>
-        public void MainWindow_Loaded(object sender, RoutedEventArgs args)
+        public async void MainWindow_Loaded(object sender, RoutedEventArgs args)
         {
             int timeout = Properties.Settings.Default.timeout;
+            if (timeout > 0)
+                wrongAuthCount = 3;
+            await AttemptsTimer(timeout);
         }
 
         /// <summary>
@@ -48,9 +51,34 @@ namespace WPFApp
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void ButtonOnClick(object sender, RoutedEventArgs e)
+        private async void ButtonOnClick(object sender, RoutedEventArgs e)
         {
+            errorLabel.Text = null;
+            if (string.IsNullOrEmpty(authLogin.Text) || string.IsNullOrEmpty(authPassword.Text))
+                errorLabel.Text = "Заполните Логин и Пароль!";
+            else
+            {
+                if (wrongAuthCount < 3)
+                    ++wrongAuthCount;
+                else
+                    await AttemptsTimer(60);
+            }
+        }
 
+        /// <summary>
+        /// Блокировка входа на определенное время.
+        /// </summary>
+        /// <param name="sec">Timeout seconds<see cref="int"/>.</param>
+        /// <returns>The <see cref="Task"/>.</returns>
+        private async Task AttemptsTimer(int timeout)
+        {
+            errorLabel.Text = $"Совершено {wrongAuthCount} неудачных попытки входа. Вход заблокирован на {timeout} секунд";
+            authButton.IsEnabled = false;
+            while (timeout > 0)
+                timeout = await UpdateTimeout(timeout);
+            errorLabel.Text = null;
+            authButton.IsEnabled = true;
+            wrongAuthCount = 0;
         }
 
         private async Task<int> UpdateTimeout(int timeout)
