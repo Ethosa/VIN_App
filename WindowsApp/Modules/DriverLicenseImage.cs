04a64ac2f7bd9447f426fbe1d0d1aad6e95da9ff
Ethosa <social.ethosa@gmail.com>
2021-11-12 09:23:07 +0700

    add DriverLicenseImage

---
+++ WindowsApp/Modules/DriverLicenseImage.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace WindowsApp.Modules
+{
+    class DriverLicenseImage
+    {
+        private Bitmap bitmap;
+        private Graphics graphics;
+
+        /// <summary>
+        /// Создает картинку для дальнейшей работы с ней.
+        /// </summary>
+        /// <param name="filename"></param>
+        public DriverLicenseImage(string filename)
+        {
+            bitmap = new Bitmap(filename);
+            graphics = Graphics.FromImage(bitmap);
+        }
+
+
+    }
+}
